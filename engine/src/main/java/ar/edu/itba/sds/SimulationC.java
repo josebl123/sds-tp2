0e194871fb0fe7ff7d75611904542ec51dbffd6b
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2026-03-27 11:00:50 -0300

    fix

--- engine/src/main/java/ar/edu/itba/sds/SimulationC.java
+++ engine/src/main/java/ar/edu/itba/sds/SimulationC.java
@@ -69,8 +69,7 @@ public class SimulationC {
             for (double o : orders) {
                 desvioSum += Math.pow((o - orderAvg), 2);
             }
-            double desvio = Math.sqrt(desvioSum / ITERATIONS_C);
-            double error = desvio / Math.sqrt(ITERATIONS_C);
+            double error = Math.sqrt(desvioSum / ITERATIONS_C);
             resultsOrder.put(eta, orderAvg);
             resultsError.put(eta, error);
         }
